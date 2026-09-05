77964e76ac7245aedf12c5ac1b36ee6bed4aeeb8
onmaxxstudio <onmaxxstudio@gmail.com>
2026-09-05 17:47:33 -0400

    Update welcome_screen.dart for entry flow

--- app/lib/screens/welcome_screen.dart
+++ app/lib/screens/welcome_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'create_account_screen.dart';
+import 'sign_in_screen.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
@@ -79,7 +80,15 @@ class WelcomeScreen extends StatelessWidget {
                             },
                           ),
                           const SizedBox(height: 12),
-                          const _SignInButton(),
+                          _SignInButton(
+                            onPressed: () {
+                              Navigator.of(context).push(
+                                MaterialPageRoute<void>(
+                                  builder: (_) => const SignInScreen(),
+                                ),
+                              );
+                            },
+                          ),
                           const SizedBox(height: 22),
                           const _PrivacyMessage(),
                         ],
@@ -329,7 +338,9 @@ class _PrimaryButton extends StatelessWidget {
 }
 
 class _SignInButton extends StatelessWidget {
-  const _SignInButton();
+  const _SignInButton({required this.onPressed});
+
+  final VoidCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
@@ -337,7 +348,7 @@ class _SignInButton extends StatelessWidget {
       width: double.infinity,
       height: 57,
       child: OutlinedButton(
-        onPressed: () {},
+        onPressed: onPressed,
         style: OutlinedButton.styleFrom(
           foregroundColor: WelcomeScreen._green,
           side: const BorderSide(color: WelcomeScreen._green, width: 1.25),
