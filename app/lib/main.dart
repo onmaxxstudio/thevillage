28bc15295be983ebf62a5ac78a08fb82fa5f7935
onmaxxstudio <onmaxxstudio@gmail.com>
2026-09-08 06:53:56 -0400

    Enable HTML semantics for reliable iPhone taps

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/semantics.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'firebase_options.dart';
@@ -20,6 +22,9 @@ Future<void> main() async {
     // AuthService shows a clear setup error instead of bypassing sign-in.
   }
   runApp(const MyApp());
+  if (kIsWeb) {
+    SemanticsBinding.instance.ensureSemantics();
+  }
 }
 
 class MyApp extends StatelessWidget {
