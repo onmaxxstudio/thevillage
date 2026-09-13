95ec68c0440200f247dea15267497d1479a97fd7
onmaxxstudio <onmaxxstudio@gmail.com>
2026-09-13 08:35:40 -0400

    Add moderation screen shell

---
+++ app/lib/screens/admin_moderation_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AdminModerationScreen extends StatelessWidget {
+  const AdminModerationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Village Moderation')),
+    );
+  }
+}
